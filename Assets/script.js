89d260d1736bb871c895d57fab8fa34a579cd347
bentwhiskers <abbott1414@gmail.com>
2023-03-30 22:45:12 -0400

    changed 'timer' to '100' in HTML; completed timer feature on start; added qanda object to JS file

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,58 @@
-// This program will create a timed quiz about coding.
-// User hits a start button which starts the timer countdown.
 // First question pops up after timer starts. 
+// Questions/Answers defined as objects
 // After question is answered, another question pops up.
 // If question is answered incorrectly, time is subtracted from the clock.
 // If all questions are answered or time reaches 0, then game is over. 
 // When game is over, score can be saved in "highscores" area.
 
 
+var qanda = {
+    qOne: "What House is Harry Potter in?",
+    aOne: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+    qTwo: "What is Harry's cousin's name?",
+    aTwo: ["Diggles", "Doofy", "Derrick", "Dudley"],
+    qThree: "What kind of pet does Harry own?",
+    aThree: ["Bat", "Owl", "Cat", "Toad"],
+    qFour: "Which of these is NOT one of Albus Dumbledore's middle names?",
+    aFour: ["Percival", "Wulfric", "Montgomery", "Brian"],
+    qFive: "What is the name of the school Harry attends?",
+    aFive: ["Hogwash", "Hogwarts", "Warthog", "Pigfarts"],
+
+};
 
 var timeEl = document.getElementById("timer");
 var startBtn = document.getElementById("btn");
+console.log(startBtn)
 
-
-function startTimer () {
+function startQuiz () {
 
     var secondsLeft = 100;
 
     var timeInterval = setInterval(function() {
-        if (secondsLeft > 0) {
+        if (secondsLeft > 1) {
             timeEl.textContent = secondsLeft;
             secondsLeft--;
-        } else (secondsLeft === 0) {
+        } else {
             timeEl.textContent = " ";
             clearInterval(timeInterval);
         }
         
     }, 1000);
-}
+};
+
+startBtn.addEventListener("click", startQuiz);
+
+var qanda = {
+    qOne: "What House is Harry Potter in?",
+    aOne: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+    qTwo: "What is Harry's cousin's name?",
+    aTwo: ["Diggles", "Doofy", "Derrick", "Dudley"],
+    qThree: "What kind of pet does Harry own?",
+    aThree: ["Bat", "Owl", "Cat", "Toad"],
+    qFour: "Which of these is NOT one of Albus Dumbledore's middle names?",
+    aFour: ["Percival", "Wulfric", "Montgomery", "Brian"],
+    qFive: "What is the name of the school Harry attends?",
+    aFive: ["Hogwash", "Hogwarts", "Warthog", "Pigfarts"],
+
+};
+
