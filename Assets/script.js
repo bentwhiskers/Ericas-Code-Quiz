2cf3dd9dda0c91ea1ff12fe45b2c088a812d9dd2
bentwhiskers <abbott1414@gmail.com>
2023-04-13 17:45:32 -0400

    added a few HTML elements for a highscore button, and a form to save initials in. Updated README. Rewrote questions in JS as objects within an array; defined several variables; wrote function for showing questions; wrote functions for correct and wrong answers; started endgame function; started function for showing scores; added event listener for endgame function.

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,49 @@
-// When I click on start, the timer starts and the first question pops up.
-// After question is answered, another question pops up.
-// If question is answered incorrectly, time is subtracted from the clock.
+
+ 
+
 // If all questions are answered or time reaches 0, then game is over. 
 // When game is over, score can be saved in "highscores" area.
 
-
-var qanda = {
-    qOne: "What House is Harry Potter in?",
-    oOne: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
-    aOne: "Gryffindor",
-    qTwo: "What is Harry's cousin's name?",
-    oTwo: ["Diggles", "Doofy", "Derrick", "Dudley"],
-    aTwo: "Dudley",
-    qThree: "What kind of pet does Harry own?",
-    oThree: ["Bat", "Owl", "Cat", "Toad"],
-    aThree: "Owl",
-    qFour: "Which of these is NOT one of Albus Dumbledore's middle names?",
-    oFour: ["Percival", "Wulfric", "Montgomery", "Brian"],
-    aFour: "Montgomery",
-    qFive: "What is the name of the school Harry attends?",
-    oFive: ["Hogwash", "Hogwarts", "Warthog", "Pigfarts"],
-    aFive: "Hogwarts",
-
-};
+var questions = [
+    {
+        q: "What House is Harry Potter in?",
+        o: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+        a: "Gryffindor",
+    }, {
+        q: "What is Harry's cousin's name?",
+        o: ["Diggles", "Doofy", "Derrick", "Dudley"],
+        a: "Dudley",
+    }, {
+        q: "What kind of pet does Harry own?",
+        o: ["Bat", "Owl", "Cat", "Toad"],
+        a: "Owl",
+    }, {
+        q: "Which of these is NOT one of Albus Dumbledore's middle names?",
+        o: ["Percival", "Wulfric", "Montgomery", "Brian"],
+        a: "Montgomery",
+    }, {
+        q: "What is the name of the school Harry attends?",
+        o: ["Hogwash", "Hogwarts", "Warthogs", "Pigfarts"],
+        a: "Hogwarts",
+    }
+];
 
 var quiz = document.getElementById("qanda");
 var timeEl = document.getElementById("timer");
 var startBtn = document.getElementById("btn");
+var intro = document.getElementById("intro");
 var finScore = document.getElementById("finalscore");
 var highScore = document.getElementById("highscores");
+var qCounter = 0;
+var statusDiv = document.getElementById("status");
+var secondsLeft = 100;
+var initials = document.getElementById("initials");
+var saveBtn = document.getElementById("save");
+var scoreList = JSON.parse(localStorage.getItem("highscores")) || [];
+
+
 
 function startTimer () {
-
-    var secondsLeft = 100;
 
     var timeInterval = setInterval(function() {
         if (secondsLeft > 1) {
@@ -44,13 +55,84 @@ function startTimer () {
         }
         
     }, 1000);
-    
+
 };
 
 function startQuiz() {
    
+    document.getElementById("intro").style.display = "none";
+    
+    showQuestions();
+};
+
+function showQuestions() {
+    var question = document.createElement('h3');
+    question.textContent = questions[qCounter].q;
+    quiz.innerHTML = " ";
+    quiz.append(question);
+    for (var i = 0; i < questions[qCounter].o.length; i++) {
+        var options = document.createElement('button');
+        options.textContent = questions[qCounter].o[i];
+        quiz.append(options);
+        options.addEventListener("click", function(event) {
+            if (event.target.textContent !== questions[qCounter].a) {
+                wrongAnswer();
+            } else if (event.target.textContent == questions[qCounter].a && event.target.textContent == questions[4].a) {
+                endGame();
+            } else {
+                correctAnswer();
+            }
+        });
+    };
+
+
+};
+// After that you want the user to click on one of the answer buttons, you need to check if that answer is correct or not, and run a function accordingly.
+function correctAnswer() {
+    var correct = document.createElement("p");
+    correct.textContent = "Correct!";
+    statusDiv.innerHTML = " ";
+    statusDiv.append(correct);
+    qCounter++;
+    showQuestions();
+};
+
+function wrongAnswer() {
+    var wrong = document.createElement("p");
+    wrong.textContent = "Wrong!";
+    statusDiv.innerHTML = " ";
+    statusDiv.append(wrong);
+    secondsLeft -= 10;
+
+};
+
+function endGame() {
+    var data = {
+        initials: initials.value,
+        score: secondsLeft,
+    };
+    scoreList.push(data);
+
+    localStorage.setItem("highscores", JSON.stringify(scoreList));
+
+    document.getElementById("qanda").style.display = "none";
+
+    document.getElementById("status").style.display = "none";
+
+    
+
+
     
 }
+// function to show highscores in a list using a for loop over the scoreList
+function showScoresList() {
+    for (var i = 0; i < scoreList.length; i++) {
+        var list = document.createElement("li");
+        list.textContent = scoreList[i];
+    }
+}
+
 
 startBtn.addEventListener("click", startTimer);
 startBtn.addEventListener("click", startQuiz);
+saveBtn.addEventListener("click", endGame);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Ericas-Code-Quiz
+# Harry Potter Quiz
+
+## Description
+This is a timed quiz designed around J.K. Rowling's Harry Potter. Correct answers result in the next question appearing; wrong answers result in a 10 second time penalty. The score is equivalent to the time remanining at the end of the quiz.
+
+## Usage
+To use, go to: https://bentwhiskers.github.io/Harry-Potter-Quiz/
+
+<insert screenshot here>
+
+## Credits
+
+## License
+MIT License
